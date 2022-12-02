35a126c6ffd1db32fd89f25a50844f39daa52fdf
kimiaGF <89272851+kimiaGF@users.noreply.github.com>
2022-12-02 18:36:27 -0500

    data prep added

---
+++ data_prep/data_prep.py
@@ -0,0 +1,194 @@
+#%%
+from dpdata import MultiSystems,LabeledSystem
+import os 
+import random
+import numpy as np
+import shutil
+#%%
+
+def OUTCAR_to_ms(parent_dir,sub_dirs=[''],run_types=['all'],flags=['OUTCAR']):
+    """Converts OUTCAR files to MultiSystem objects
+
+    Args:
+        parent_dir (str): path to parent directory to all raw data
+        sub_dirs (list, optional): potential existence of subdirs within parent (see Requirements). Defaults to [''].
+        run_types (list, optional): run types within parent to include in data. Defaults to ['all'].
+        flags (list, optional): flags indicating OUTCAR selection, must be file in OUTCAR folder. Defaults to ['OUTCAR'].
+
+    Raises:
+        Warning: LabeledSystem object was not able to be defined
+
+    Returns:
+        MultiSystem(): two MultiSystem() objects excluding and including virials
+        int: total frame counts for systems with and without virials
+    
+    Requirements:
+        parent_dir tree structure must follow structure:
+             parent/system/run_type/runs/OUTCAR
+             or
+             parent/system/subdir/run_type/runs/OUTCAR
+        e.g
+            B4C/B12-CBC/temperature_hold/T5/OUTCAR
+            or
+            B4C/B12-CBC/small_strains/shear_strain/S25/OUTCAR
+    """
+    #define dpdata multisystem objects 
+    ms_virial = MultiSystems()
+    ms = MultiSystems()
+
+    #keep count of number of frames in each run type
+    counts_virial = {key: 0 for key in run_type}
+    counts_novirial = {key: 0 for key in run_type}
+
+    logs = []
+
+    #scrub through defined parent directory
+    for s in sub_dirs:
+        #redefine parent dir 
+        par = os.path.join(parent_dir,s)
+
+        for r,d,f in os.walk(par):
+            #extract information from path name 
+            run_type,system,conditions = r.split('/')[-4:]
+
+            #define conditions for directory for extraction of OUTCAR
+            hasflag = not set(flags).isdisjoint(f)
+            isoutcar = 'OUTCAR' in f
+
+            if 'all' in run_types:
+                inruntype=True
+            else:
+                inruntype = run_type in run_types
+            
+            if isoutcar and hasflag and inruntype:
+                #get OUTCAR file path
+                file_path = os.path.join(r,'OUTCAR')
+
+                try:
+                    #define labeled system with current OUTCAR
+                    ls = LabeledSystem(file_path)
+                except:
+                    raise Warning(f'something is wrong with {r}')
+                
+                #separates data with and without virial stresses 
+                if 'virials' in ls.data.keys() and len(ls) > 0:
+                    ms_virial.append(ls)
+                    counts_virial[system]+=ls.get_nframes()
+                    logs.append(r)
+                else:
+                    ms.append(ls)
+                    counts_novirial[system]+=ls.get_nframes()
+                    logs.append(r)
+    return ms,ms_virial,counts_novirial,counts_virial
+#function for finding all non-training indices in splitting 
+def find_missing(sub_list,full_list):    
+    return [x for x in range(1, full_list) if x not in sub_list]
+
+def train_test_split(destination_dir,train_split=0.9,ms_virial,ms):
+    """scrubs destination dir for .npy files and splits into train-test sets
+
+    Args:
+        destination_dir (str): path to destination directory to write training and validation folders.
+        train_split (float,optional): proportion of data to reserve for training. Defaults to 0.9.
+        ms_virial (MultiSystem()): MultiSystem() object containing systems with virials.
+        ms (MultiSystem()): MultiSystem() object containing systems without virials.
+    """
+    #requires that .npy are already in destination dir tree
+    #walk destination_dir
+    for r,d,f in os.walk(destination_dir):
+        #check if training or validation already exists
+        isSplit = r.split('/')[-3] == 'training_data' or r.split('/')[-3] == 'validation_data'
+        
+        #check if we are in correct dir depth (with .npy files)
+        if 'box.npy' in f and not isSplit:
+            #makes training and validation dirs ./training/system/set.000/...
+            ptrain = os.path.join(destination_dir,r.split('/')[-3],'training_data',*r.split('/')[-2:])
+            ptest = os.path.join(destination_dir,r.split('/')[-3],'validation_data',*r.split('/')[-2:])
+            
+            #if train,val directories don't exist, make them
+            if not os.path.isdir(ptrain):
+                os.makedirs(ptrain)
+            if not os.path.isdir(ptest):
+                os.makedirs(ptest)
+                
+            #pick out all npy files for train-test splitting 
+            files = [i for i in f if i.endswith('npy')]
+            
+            #define system 
+            system = r.split('/')[-2]
+            
+            #find total number of frames 
+            hasvirials = r.split('/')[-3] == 'virials'
+            if hasvirials:
+                n_frames = ms_virial[system].get_nframes()
+            else:
+                n_frames = ms[system].get_nframes()
+            
+            #find number of training frames 
+            n_train = int(train_split * n_frames)
+            
+            #randomly choose training n_train indeces
+            train_frames = random.sample(range(0,n_frames),n_train)
+
+            #extract missing frames not chosen for training (for validation)
+            test_frames = find_missing(train_frames,n_frames)
+            
+            #loops through all .npy's in r and adds them to dataframe 
+            for file in files:
+                #gets path to .npy file
+                file_path = os.path.join(r,file)
+                
+                #load npy data
+                with open(file_path):
+                    data = np.load(file_path)
+                
+                #extract training and validation frames from data
+                train_data = data[train_frames]
+                test_data = data[test_frames]
+                
+                #define path for training and validation data file
+                train_file = os.path.join(ptrain,file)
+                test_file = os.path.join(ptest,file)
+                
+                #save split data into filename and path
+                np.save(train_file,train_data)
+                np.save(test_file,test_data)
+            
+            #copy type_map.raw and type.raw to train-test directories
+            type_map = r+'/../type_map.raw'
+            type_raw = r+'/../type.raw'
+            
+            shutil.copy(type_map,ptrain+'/..')
+            shutil.copy(type_raw,ptrain+'/..')
+            shutil.copy(type_map,ptest+'/..')
+            shutil.copy(type_raw,ptest+'/..')
+
+def OUTCAR_to_npy(parent_dir,destination_dir,sub_dirs=[''],run_types=['all'],flags=['OUTCAR'],train_split=0.9):
+    """reads in data from OUTCAR output from VASP and converts to .npy files in training and validation folders.
+
+    Args:
+        parent_dir (str): path to parent directory to all raw data
+        destination_dir (str): path to destination directory to write training and validation folders
+        sub_dirs (list, optional): potential existence of subdirs within parent (see Notes). Defaults to [''].
+        run_types (list, optional): run types within parent to include in data. Defaults to ['all'].
+        flags (list, optional): flags indicating OUTCAR selection, must be file in OUTCAR folder. Defaults to ['OUTCAR'].
+        train_split (float, optional): proportion of data to reserve for training. Defaults to 0.9.
+
+    Raises:
+        Exception: if given parent directory does not exist
+    """
+    
+    #check if destination directory exists
+    if not os.path.isdir(destination_dir):
+        os.makedirs(destination_dir)
+
+    #check if parent dir exists
+    if not os.path.isdir(parent_dir):
+        raise Exception("parent directory does not exist.")
+    
+    ms,ms_virial = OUTCAR_to_ms(parent_dir=parent_dir,sub_dirs=sub_dirs,run_types=run_types,flags=flags)
+
+    train_test_split(destination_dir=destination_dir,train_split=train_split,ms_virial=ms_virial,ms=ms)
+
+
+# %%
